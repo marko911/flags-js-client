59657290d7e8b8a81ee2005db469f10ff77faec6
Marko <marko.barca10@gmail.com>
2020-09-11 13:45:51 -0400

    fix base url address

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = `${process.env.EVALS_ENDPOINT}/evals/flags/`;
+export const BASE_URL = "https://staging.markob.io/evals/flags/";
 export const ON_FLAG = "On";
 export const OFF_FLAG = "Off";
